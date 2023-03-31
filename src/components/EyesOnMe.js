ece995b580f822ed2eb6cfa44f468c896f839a0c
Ian Flurkey <ianflurkey@gmail.com>
2023-03-31 13:48:47 -0400

    commit tree c2446d2cd293bcbf8df89d03ad394427df2db437

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function blur(event){
+        console.log("Hey! Eyes on me!")
+    }
+    function focus(event){
+        console.log("Good!")
+    }
+
+
+    return <button onBlur={blur} onFocus={focus}>Eyes on me</button>
+}
+
+export default EyesOnMe
